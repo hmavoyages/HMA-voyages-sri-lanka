e1cc3ea44efcc9917a335aa8f537cf80c5710747
Vinuja Ransith <hmavoyageslk@gmail.com>
2025-10-01 22:40:46 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1200, // raise limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          i18n: ["i18next", "react-i18next"]
+        }
+      }
+    }
+  }
+});
